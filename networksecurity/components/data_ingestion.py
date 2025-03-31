469f43a34cff09fe7a53af34233053ccebabe7be
Ram Govind V <ramgovindvi.in@gmail.com>
2025-04-01 00:25:24 +0530

    setting up data ingestion component

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -48,8 +48,32 @@ class DataIngestion:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
+    def split_data_as_train_test(self, dataframe: pd.DataFrame):
+        try:
+            train_set,test_set = train_test_split(
+                dataframe,self.data_ingestion_config.train_test_split_ratio
+            )
+            logging.info("Performed train test split on the dataframe")
+            logging.info("Exited split_data_as_train_test method of Data Ingestion class")
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            
+            logging.info("Exporting train and test file path")
+
+            train_set.to_csv(
+                self.data_ingestion_config.training_file_path, index = False, header = True
+            )
+            test_set.to_csv(
+                self.data_ingestion_config.testing_file_path, index = False, header = True
+            )
+            logging.info("Exported train and test file path")
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
     def initiate_data_ingestion(self):
         try:
-            dataframe = self.export_collection_as_dataframe
+            dataframe = self.export_collection_as_dataframe()
+            dataframe = self.export_data_into_feature_store(dataframe)
+            self.split_data_as_train_test(dataframe)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
